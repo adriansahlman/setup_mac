6b393a1c1cf7193ca9b60bcbc0b84b5ea3188dfa
Adrian Sahlman <7781148+adriansahlman@users.noreply.github.com>
2022-11-01 12:47:02 +0200

    Update add_python.sh

--- add_python.sh
+++ add_python.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 set -eux
 ARCH=arm64
 PYTHON_VERSION=3.8
 curl -fsSL -v -o ~/anaconda.sh -O  "https://repo.anaconda.com/archive/Anaconda3-2022.10-MacOSX-${ARCH}.sh"
 chmod +x ~/anaconda.sh
 ~/anaconda.sh
-#rm ~/anaconda.sh
-#echo "export PATH=${HOME}/anaconda3/bin:\${PATH}" >> ~/.zshrc
-#export PATH=${HOME}/anaconda3/bin:${PATH}
-~/anaconda3/bin/conda install -y python=${PYTHON_VERSION} cmake conda-build pyyaml numpy ipython
+rm ~/anaconda.sh
+source ~/.zshrc
+conda install -y python=${PYTHON_VERSION} cmake conda-build pyyaml numpy ipython
 conda install -y pytorch torchvision torchaudio -c pytorch
 pip install --upgrade pip
