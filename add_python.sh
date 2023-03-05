9a324e8dbd28a83c22949cca141706461546e9e1
Adrian Sahlman <adrian.sahlman@gmail.com>
2023-03-05 08:09:15 -0300

    python

---
+++ add_python.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+set -eux
+ARCH=arm64
+PYTHON_VERSION=3.8
+#curl -fsSL -v -o ~/anaconda.sh -O  "https://repo.anaconda.com/archive/Anaconda3-2022.10-MacOSX-${ARCH}.sh"
+#chmod +x ~/anaconda.sh
+#~/anaconda.sh
+#rm ~/anaconda.sh
+#source ~/.zshrc
+conda install -y python=${PYTHON_VERSION} cmake conda-build pyyaml numpy ipython
+conda install -y pytorch torchvision torchaudio -c pytorch
+pip install --upgrade pip
