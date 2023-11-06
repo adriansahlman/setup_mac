a38a9abf21037bccb023a45779763408eac2bb62
Adrian Sahlman <7781148+adriansahlman@users.noreply.github.com>
2023-11-06 09:55:23 -0300

    Update setup.part2.sh

--- setup.part2.sh
+++ setup.part2.sh
@@ -115,10 +115,14 @@ git config --global core.editor vim
 
 
 
-# FIX FOR PYTHON GRPC
-
+# PYTHON FIXES FOR INSTALLING SOME PACKAGES
 echo 'export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1' >> ~/.zshrc
 echo 'export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1' >> ~/.zshrc
+
+# these 2 probably only works for arch=arm64
+echo 'export HDF5_DIR=/opt/homebrew/opt/hdf5' >> ~/.zshrc
+echo 'export BLOSC_DIR=/opt/homebrew/opt/c-blosc' >> ~/.zshrc
+
 
 # INSTALL PYTHON AND POETRY
 brew install python
@@ -129,3 +133,5 @@ poetry completions zsh > ~/.zfunc/_poetry
 echo 'fpath+=~/.zfunc' >> ~/.zshrc
 echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
 echo 'export POETRY_VIRTUALENVS_IN_PROJECT=true' >> ~/.zshrc
+
+
