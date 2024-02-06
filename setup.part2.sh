fea30f9b82ffb8f4b5a07c1d8383dbfc02abbec2
Adrian Sahlman <adrian.sahlman@mail.weir>
2024-02-06 11:48:25 +0100

    update

--- setup.part2.sh
+++ setup.part2.sh
@@ -104,24 +104,3 @@ git config --global init.defaultBranch main
 git config --global user.name "Adrian Sahlman"
 git config --global user.email "adrian.sahlman@gmail.com"
 git config --global core.editor vim
-
-
-
-# PYTHON FIXES FOR INSTALLING SOME PACKAGES
-echo 'export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1' >> ~/.zshrc
-echo 'export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1' >> ~/.zshrc
-
-# these 2 probably only works for arch=arm64
-echo 'export HDF5_DIR=/opt/homebrew/opt/hdf5' >> ~/.zshrc
-echo 'export BLOSC_DIR=/opt/homebrew/opt/c-blosc' >> ~/.zshrc
-
-
-# INSTALL PYTHON AND POETRY
-brew install "python@3.11"
-curl -sSL https://install.python-poetry.org | sed 's/symlinks=False/symlinks=True/' | python -
-echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.zshrc
-mkdir -p ~/.zfunc
-poetry completions zsh > ~/.zfunc/_poetry
-echo 'fpath+=~/.zfunc' >> ~/.zshrc
-echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
-echo 'export POETRY_VIRTUALENVS_IN_PROJECT=true' >> ~/.zshrc
--- setup.private.sh
+++ setup.private.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+set -eux
+
 brew install --cask steam
 brew install --cask deluge
 brew install --cask whatsapp
